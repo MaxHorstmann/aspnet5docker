4d26bb1c3c3917ba61681d41bc8d497ae61534cc
Max Horstmann <max.horstmann@gmail.com>
2016-01-02 21:46:43 -0500

    experimental postgres

--- src/AspNet5Docker/Controllers/LocationController.cs
+++ src/AspNet5Docker/Controllers/LocationController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AspNet5Docker.Models;
 using Microsoft.AspNet.Mvc;
+using Npgsql;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,18 +12,33 @@ namespace AspNet5Docker.Controllers
 {
     public class LocationController : Controller
     {
-        static readonly List<Location> Locations  = new List<Location>(); 
 
         public IActionResult Index()
         {
+            var locations = new List<Location>();
+            using (var conn = new NpgsqlConnection("Host=192.168.1.8;Username=postgres;Password=mysecretpassword;Database=postgres"))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT name FROM Locations";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            locations.Add(reader.GetString(0));
+                        }
+                    }
+                }
+            }
 
-
-            return View(Locations);
+            return View(locations);
         }
 
         public IActionResult Add(string newLocation)
         {
-            Locations.Add(new Location() { Name = newLocation } );
+            // TODO // Locations.Add(new Location() { Name = newLocation } );
             return RedirectToAction("Index");
         }
     }
