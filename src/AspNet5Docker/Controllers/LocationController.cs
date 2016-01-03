af861be07183ba9ff59b0997907fc3cf2a55b99a
Max Horstmann <max.horstmann@gmail.com>
2016-01-02 20:04:48 -0500

    do some stuff with models

--- src/AspNet5Docker/Controllers/LocationController.cs
+++ src/AspNet5Docker/Controllers/LocationController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNet5Docker.Models;
 using Microsoft.AspNet.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,7 +14,12 @@ namespace AspNet5Docker.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var locations = new List<Location>();
+            locations.Add(new Location() {Name = "Zurich"});
+            locations.Add(new Location() { Name = "Berlin" });
+            locations.Add(new Location() { Name = "New York" });
+
+            return View(locations);
         }
     }
 }
